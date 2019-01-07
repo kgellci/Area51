a411b3fabf2ac91159769e2a239d730834b8b57a
RJ-Clegg <Robert.clegg@gmail.com>
2019-01-07 20:01:28 +0200

    Issues-30

--- Area51/Sources/FeedViewController.swift
+++ Area51/Sources/FeedViewController.swift
@@ -3,12 +3,13 @@ import SafariServices
 import UIKit
 
 final class FeedViewController: UIViewController {
+
     @IBOutlet weak var tableView: UITableView!
+
     private var dataSource: ListingsDataSource! {
         didSet {
             self.dataSource.updated = { [weak self] in
-                self?.tableView.refreshControl?.endRefreshing()
-                self?.tableView.reloadData()
+                self?.endRefreshingOnTableView()
             }
         }
     }
@@ -16,30 +17,46 @@ final class FeedViewController: UIViewController {
     class func instantiateFromStoryboard(withSubreddit subreddit: Subreddit) -> FeedViewController {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let feedViewController: FeedViewController = storyboard.viewControllerFrom(identifier: "FeedViewController")
+
         feedViewController.dataSource = ListingsDataSource(subreddit: subreddit)
         feedViewController.title = subreddit.name
+
         return feedViewController
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupTableView()
+        setupTableView()
     }
 
     private func setupTableView() {
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-        self.tableView.refreshControl = UIRefreshControl()
-        self.tableView.refreshControl?.addTarget(self, action: #selector(self.refreshData), for: .valueChanged)
-        self.tableView.register(UINib(nibName: "ListingTableViewCell", bundle: nil),
-                                forCellReuseIdentifier: "ListingTableViewCell")
-        self.tableView.register(UINib(nibName: "ListingThumbnailTableViewCell", bundle: nil),
-                                forCellReuseIdentifier: "ListingThumbnailTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupTableViewRefreshControl()
+        registerTableViewCells()
+    }
+
+    private func setupTableViewRefreshControl () {
+        tableView.refreshControl = UIRefreshControl()
+        tableView.refreshControl?.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+    }
+
+    private func registerTableViewCells () {
+        tableView.register(UINib(nibName: "ListingTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "ListingTableViewCell")
+
+        tableView.register(UINib(nibName: "ListingThumbnailTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "ListingThumbnailTableViewCell")
     }
 
     @objc
     private func refreshData() {
-        self.dataSource.refresh()
+        dataSource.refresh()
+    }
+
+    private func endRefreshingOnTableView () {
+        tableView.refreshControl?.endRefreshing()
+        tableView.reloadData()
     }
 }
 
@@ -65,8 +82,10 @@ extension FeedViewController: UITableViewDelegate, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+
         let listing = self.dataSource.listings[indexPath.row]
         let safariViewController = SFSafariViewController(url: listing.url)
-        self.present(safariViewController, animated: true, completion: nil)
+
+        present(safariViewController, animated: true, completion: nil)
     }
 }
