899479b1860e11254535ff6a3b990cd240b1fa6e
Felix Förtsch <felixfoertsch@gmail.com>
2019-01-17 20:05:26 +0100

    add tabbaricons

--- Area51/Sources/NavigationRouter.swift
+++ Area51/Sources/NavigationRouter.swift
@@ -3,10 +3,10 @@ import UIKit
 struct NavigationRouter {
     static var mainNavigation: UITabBarController {
         let feedController = UIStoryboard.feed.instantiateInitialViewController()!
-        feedController.tabBarItem = UITabBarItem(title: "Feed", image: nil, tag: 0)
+        feedController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(named: "TabbarFeedIcon"), tag: 0)
 
         let settingsController = UIStoryboard.settings.instantiateInitialViewController()!
-        settingsController.tabBarItem = UITabBarItem(title: "Settings", image: nil, tag: 0)
+        settingsController.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(named: "TabbarSettingsIcon"), tag: 0)
 
         let tabBar = UITabBarController()
         tabBar.viewControllers = [feedController, settingsController]
