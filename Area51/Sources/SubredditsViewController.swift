34976131d7e17ec67e36bdcd7d4a684d6cb29c32
Dulitha Dabare <duli.dabare@gmail.com>
2019-02-20 22:10:03 +0530

    Add SubredditSections enum as extension
    The SubredditSections enum was only being used inside the SubredditsViewController
    extension. Because of this, the enum file was deleted and the enum was added
    inside the extension.

--- Area51/Sources/SubredditsViewController.swift
+++ Area51/Sources/SubredditsViewController.swift
@@ -34,6 +34,23 @@ class SubredditsViewController: UIViewController {
 }
 
 extension SubredditsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    enum SubredditSections: Int {
+        case redditFeeds = 0
+        case defaultFeeds = 1
+        
+        public var title: String {
+            switch self {
+            case .redditFeeds:
+                return "Reddit Feeds"
+            case .defaultFeeds:
+                return "Default Feeds"
+            }
+        }
+        public static var allSections: [SubredditSections] {
+            return [.redditFeeds , .defaultFeeds]
+        }
+    }
     func numberOfSections(in tableView: UITableView) -> Int {
         return SubredditSections.allSections.count
     }
