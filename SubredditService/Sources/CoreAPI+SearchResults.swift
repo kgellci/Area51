338b5028c9c5a3ddf2454e8aed281afc0e5afef2
Kris Gellci <kgellci@gmail.com>
2019-06-02 13:06:34 -0700

    Update to Swift 5

--- SubredditService/Sources/CoreAPI+SearchResults.swift
+++ SubredditService/Sources/CoreAPI+SearchResults.swift
@@ -2,11 +2,9 @@ import CoreAPI
 import Foundation
 
 public extension CoreAPI {
-    public static func seachPopularResults<T: Decodable>(listingRoute: APIRoute,
-                                                         value: T.Type,
-                                                         query: String,
-                                                         completion:
-        @escaping (Result<[SearchResult]>) -> Void) -> URLSessionTask {
+    static func seachPopularResults<T: Decodable>(listingRoute: APIRoute, value: T.Type, query: String,
+                                                  completion: @escaping (Result<[SearchResult]>) -> Void)
+        -> URLSessionTask {
 
         var parameters = [String: String]()
         parameters["query"] = query
