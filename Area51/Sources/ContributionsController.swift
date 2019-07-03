6953b74cd8634c8f6b8a54d625a53cba95bcb5bc
Yon Montoto <yon.montoto@gmail.com>
2019-07-03 19:02:13 -0400

    Adds adjustments from PR comments

--- Area51/Sources/ContributionsController.swift
+++ Area51/Sources/ContributionsController.swift
@@ -6,49 +6,26 @@ class ContributionController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UINib(nibName: "ContributorCell", bundle: nil), forCellReuseIdentifier: "ContributorCell")
+        tableView.register(ContributorCell.nib,
+                           forCellReuseIdentifier: ContributorCell.reuseIdentifier)
+        self.title = "Contributors"
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        DispatchQueue.global(qos: .background).async {
-            self.getContributors { (res) in
+        DispatchQueue.main.async {
+            Contributor.getContributors { (res) in
                 switch res {
                 case .success(let contributors):
                     contributors.forEach({ (contributor) in
                         self.listOfContributors.append(contributor)
                     })
+                    self.tableView.reloadData()
                 case .failure(let err):
                     print(err)
                 }
             }
         }
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        DispatchQueue.main.async {
-             self.tableView.reloadData()
-        }
-    }
-
-    fileprivate func getContributors(completion: @escaping (Result<[Contributor], Error>) -> Void) {
-        let request = "https://api.github.com/repos/kgellci/Area51/contributors?anon=1"
-        guard let url = URL(string: request) else { return }
-
-        URLSession.shared.dataTask(with: url) { (data, _, err) in
-            if let err = err {
-                completion(.failure(err))
-                return
-            }
-            guard let data = data else { return }
-
-            do {
-                let contributors = try JSONDecoder().decode([Contributor].self, from: data)
-                completion(.success(contributors))
-            } catch let jsonError {
-                completion(.failure(jsonError))
-            }
-        }.resume()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -60,11 +37,10 @@ class ContributionController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // swiftlint:disable:next force_cast
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ContributorCell", for: indexPath) as! ContributorCell
+        let cell: ContributorCell = tableView.reusableCell(forIdentifier: ContributorCell.reuseIdentifier)
         let contributors = listOfContributors[indexPath.item]
 
-        cell.contributorName.text = (contributors.login != nil) ? contributors.login : contributors.name
+        cell.contributorName.text = contributors.login ?? contributors.name
         cell.contributionsLabel.text = "\(contributors.contributions) Contributions"
 
         return cell
--- Area51/Sources/TableViewCells/ContributorCell.swift
+++ Area51/Sources/TableViewCells/ContributorCell.swift
@@ -10,4 +10,13 @@ import UIKit
 class ContributorCell: UITableViewCell {
     @IBOutlet var contributorName: UILabel!
     @IBOutlet var contributionsLabel: UILabel!
+
+    static var reuseIdentifier: String {
+        return "ContributorCell"
+    }
+
+    static var nib: UINib {
+        return UINib(nibName: "ContributorCell", bundle: nil)
+    }
+
 }
